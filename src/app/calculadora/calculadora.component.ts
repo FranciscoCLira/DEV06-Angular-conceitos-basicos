31ad7b441d8c1344fd489c8ad163625d7f26d284
Francisco Casemiro Lira <franciscoclira@hotmail.com>
2025-05-08 15:39:35 -0300

    14-Capturando valores de formulário utilizando o módulo de Formas do Angular
     Para capturar os valores dos campos precisamos de um modulo, o modulo de formulários
     Em src\app\calculadora\calculadora.component.ts
     importar: import { FormsModule } from '@angular/forms';
     ...
       imports: [ FormsModule],
     ...
           <form #frm="ngForm">
     ...  ===> abaixo referencia a variavel numero1 do ...ts
       <input type="number" id="numero1" name="numero1" [(ngModel)]="numero1" />
    
       OBS: [(ngModel)]  ==> entre colchetes indica uma diretiva de "two-way data bidind,
                             recebe e envia os dados da tela
            (click)=...  ==> já aqui é one-way data biding ==> apenvas envia no clic

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculadora',
-  imports: [],
+  imports: [ FormsModule],
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.scss'
 })
@@ -17,6 +18,7 @@ export class CalculadoraComponent {
     console.log('Número 1: ', this.numero1);
     console.log('Número 2: ', this.numero2);  
     this.resultado = this.numero1 + this.numero2;
+    console.log('Resultado: ', this.resultado);
   }
 
 }
