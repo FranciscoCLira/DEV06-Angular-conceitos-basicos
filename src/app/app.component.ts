1bc982d4f3b755e3bbd4ef6e838ba40578c02fef
Francisco Casemiro Lira <franciscoclira@hotmail.com>
2025-05-08 13:24:24 -0300

    12-Criando uma Calculadora
    12  Criando uma Calculadora
    
    ) Abrir o terminal no VSCode
    ) Gerar novo componente
       Samsung@LAPTOP-9P7FQEU6 MINGW64 /c/DEV06-Angular/projetos/conceitos-basicos (main)
       $ ng generate component calculadora
       CREATE src/app/calculadora/calculadora.component.html (27 bytes)
       CREATE src/app/calculadora/calculadora.component.spec.ts (650 bytes)
       CREATE src/app/calculadora/calculadora.component.ts (246 bytes)
       CREATE src/app/calculadora/calculadora.component.scss (0 bytes)
    
       Samsung@LAPTOP-9P7FQEU6 MINGW64 /c/DEV06-Angular/projetos/conceitos-basicos (main)
       $
    
    3) Em src\app\app.component.ts  importar:
       import { CalculadoraComponent } from './calculadora/calculadora.component';
    
       @Component({
           selector: 'app-root',
           imports: [ CalculadoraComponent ],
           ...
    
    4) Em src\app\app.component.html  incluir inicialmente:
           <app-calculadora></app-calculadora>
    
    5) Salvar e compilar
       Samsung@LAPTOP-9P7FQEU6 MINGW64 /c/DEV06-Angular/projetos/conceitos-basicos (main)
       $ ng serve
         ➜  Local:   http://localhost:4200/
    
    6) Começar o codigo da calculadora em: src\app\calculadora\calculadora.component.html
       substituindo a tag de teste ==> <p>calculadora works!</p>
    
    7) Fazer o CSS  em  src\app\calculadora\calculadora.component.scss
    
    Assim esta pronto a pagina do html + css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MinhapaginaComponent } from './minhapagina/minhapagina.component';
-import { HelloWorldComponent } from './helloworld/helloworld.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HelloWorldComponent, MinhapaginaComponent],
+  imports: [ CalculadoraComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
